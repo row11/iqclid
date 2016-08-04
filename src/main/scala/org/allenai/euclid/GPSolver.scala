290104c9d72ddf62c27e0250a46271061d1bda07
cristipp@allenai.org <cristipp@allenai.org>
2016-08-04 10:36:21 -0700

    readdy to roll, maybe

--- src/main/scala/org/allenai/euclid/GPSolver.scala
+++ src/main/scala/org/allenai/euclid/GPSolver.scala
@@ -2,8 +2,13 @@ package org.allenai.euclid
 
 import org.allenai.euclid.api.{ Fitness, Solution, Solver }
 
-/** Created by cristipp on 8/4/16.
-  */
+object GPSolver {
+  def main(args: Array[String]) = {
+    println("Hello world from Scala!")
+    println(clj.Bridge.greet)
+  }
+}
+
 class GPSolver(fitness: Fitness) extends Solver {
   override def solve(s: NumberSequence): Seq[Solution] = ???
 }
--- src/main/scala/org/allenai/euclid/Main.scala
+++ src/main/scala/org/allenai/euclid/Main.scala
@@ -3,9 +3,6 @@ package org.allenai.euclid
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("Hello world from Scala!")
-    println(clj.Bridge.greet)
-
     val s = NumberSequence(Seq(0, 3, 6, 9, 12), 2)
     val search = new BaselineSearch(0.999, 1000, 30)
     val best = search.best(s)
